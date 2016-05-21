d7287f0eac092bff4934ae8d0853627d1776365c
kineticobservations <kineticobservations@gmail.com>
2016-05-21 02:12:01 -0700

    Create K2GCDTools.swift

---
+++ K2GCDTools.swift
@@ -0,0 +1,18 @@
+//
+//  K2HelperTools.swift
+//
+//  Created by Kailen & Kathryne Engel on 3/2/16.
+//  Copyright © 2016 K2. All rights reserved.
+//
+//  Grand Central Dispatch & Timing utility functions
+//
+
+// delay(3) { ... }
+func delay (delay: Double, block: ()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), block)
+}
