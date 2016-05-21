faedf7ba26b355db19ba86500cde0ac565cf951d
Kailen Engel <kineticobservations@gmail.com>
2016-05-21 02:59:29 -0700

    Add: K2GraphicsTools.swift

---
+++ K2GraphicsTools.swift
@@ -0,0 +1,23 @@
+//
+//  K2GraphicsTools.swift
+//
+//  Created by Kailen & Kathryne Engel on 3/2/16.
+//  Copyright © 2016 K2. All rights reserved.
+//
+//  Graphics utility functions
+//
+
+func addSimpleGradientToLayer (
+    layer: CALayer,
+    startColor: UIColor,
+    endColor: UIColor,
+    atIndex index: UInt32 = 0) {
+        
+    let gradientColors: [CGColor] = [startColor.CGColor, endColor.CGColor]
+    let gradient = CAGradientLayer()
+    
+    gradient.colors = gradientColors
+    gradient.frame = layer.bounds
+    
+    layer.insertSublayer(gradient, atIndex: index)
+}
