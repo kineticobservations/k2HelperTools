1a398cec338375022098234d1c723652981cb249
kineticobservations <kineticobservations@gmail.com>
2016-09-18 18:36:13 -0700

    added buildIndex to K2Helper tools
    Creates a sorted index of (Character, [String]) from a string array

--- K2GCDTools.swift
+++ K2GCDTools.swift
@@ -18,3 +18,30 @@ func delay (delay: Double, block: ()->()) {
         ),
         dispatch_get_main_queue(), block)
 }
+
+// create and sorted index of (Character, [String]) from a string array 
+typealias Entry = (Character, [String])
+
+func buildIndex(words: [String]) -> [Entry] {
+    var tempWords = words.filter { $0 != "" }
+    func distinct<T: Equatable>(source: [T]) -> [T] {
+        var unique = [T]()
+        for item in source {
+            if !unique.contains(item) {
+                unique.append(item)
+            }
+        }
+        return unique
+    }
+    
+    func firstLetter(str: String) -> Character {
+        return str[str.index(str.startIndex, offsetBy: 0)]
+    }
+    
+    return distinct(source: tempWords.map(firstLetter)).map {
+        (letter) -> Entry in
+        return (letter, tempWords.filter ({
+            firstLetter(str: $0) == letter
+        }))
+    }
+}
